36dd8ec3d41453d8ae7fa837e322384e4f760056
glacent50 <glacent50@gmail.com>
2025-08-26 12:28:09 +0900

    프로젝트 파일 및 기본 모듈 추가: SmartFlowerPot.xpr, smart_flower_pot_top.v, Basys-3-Master.xdc, project.wpc, SmartFlowerPot.lpr

---
+++ SmartFlowerPot.srcs/sources_1/new/smart_flower_pot_top.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/26/2025 12:15:02 PM
+// Design Name: 
+// Module Name: smart_flower_pot_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// 샘플로 And Gate 만듬.
+module smart_flower_pot_top(
+    input A,
+    input B,
+    output F);
+    
+    assign F = A & B;
+    
+endmodule
